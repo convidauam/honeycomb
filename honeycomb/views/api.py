78811401ee1e275004d2db8aeb299c5aa7e15a87
EduardoGog <eduardo.glez7828@gmail.com>
2025-12-02 17:17:00 -0600

    fixing PR

--- honeycomb/views/api.py
+++ honeycomb/views/api.py
@@ -163,21 +163,6 @@ class DroneResource:
             'background': user.get('background'),
         }
     
-@resource(path='/api/v1/userid', cors_origins=('*',), factory='honeycomb.root_factory')
-class UserIDResource:
-    def __init__(self, request, context=None):
-        self.request = request
-        self.context = context
-
-    def get(self):
-        user = getattr(self.request, 'identity', None)
-        if not user:
-            self.request.response.status = 401
-            return {'error': 'Unauthorized'}
-        return {
-            'userid': user.get('userid'),
-        }
-
 sipping_data_store = {}
 
 @resource(path='/api/v1/sipping/{nodeid}', cors_origins=('*',))
