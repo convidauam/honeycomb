b86c84ebbbe959eb9d9a3e0f3987fc943006118b
David Suarez Pascal <david@apuntia.com>
2026-06-17 10:56:20 -0600

    Implemented CellAudio create view using colander and blobs
    CellAudio create view uses colander to validate client data and ZODB Blob for binary data storage.
    
    NOTE: CSRF protection is on, but to test this view it could be turned off while we agreed on a standard procedure to accomplish this check.
    
    Signed-off-by: David Suarez Pascal <david@apuntia.com>

---
+++ honeycomb/forms/cells.py
@@ -0,0 +1,9 @@
+import colander
+import deform
+
+
+class AudioCellSchema(colander.MappingSchema):
+    title = colander.SchemaNode(colander.String(), missing="")
+    # data = colander.SchemaNode(deform.schema.FileData())
+    mimetype = colander.SchemaNode(colander.String(), validator=colander.OneOf(['audio/mpeg', 'audio/aac', 'audio/ogg', 'audio/wav', 'audio/flac'], msg_err='"${val}" must be one of ${choices}'))
+    parent = colander.SchemaNode(colander.String(), validator=colander.uuid, missing=None)
