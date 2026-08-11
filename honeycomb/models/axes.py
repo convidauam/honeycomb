044b171a2646d5a0d60bfe2f1313ca2c6666e49b
LuisEnriqueFC <luise@ciancias.unam.mx>
2026-08-11 17:47:05 -0600

    Mds correction to correctly add it to the Honeycomb Explorer class

--- honeycomb/models/axes.py
+++ honeycomb/models/axes.py
@@ -45,6 +45,7 @@ class HoneycombExplorer(Persistent):
         self.__hc__ = honeycomb
         self.names = []
         self.matrix = None
+        self.coords_2d= None
 
     def update_matrix(self):
         "Iterates through the beehive nodes and recalculates distance matrix based on the axes values of nodes belonging to the chosen Honeycomb"
@@ -59,11 +60,11 @@ class HoneycombExplorer(Persistent):
                     coords.append(node.__axes__)
         self.matrix = spatial.distance_matrix(np.array(coords), np.array(coords))
         self.names = names
-    def reduction_2d(self):
+    def reduction_2d(self, components=2):
         "This use the MDS dimentional reduction trough sklearn function defined"
         if self.matrix is None:
             self.update_matrix()
-        reduction= MDS(n_components=2, dissimilarity='precomputed', random_state=42)
+        reduction= MDS(n_components=components, dissimilarity='precomputed', random_state=42)
         "In reduction function precomputed indicates the property of matrix distances and no the nodes points in the space, while we use the random state to preserve the correct position of all nodes"
-        coords_2d = mds.fit_transform(self.update_matrix)
-        return coords_2d
+        self.coords_2d = reduction.fit_transform(self.update_matrix)
+        return self.coords_2d
