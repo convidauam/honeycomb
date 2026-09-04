fcdb3c5e7b38e506f9c82cb583681ba2bcb459fe
David Suarez Pascal <david@apuntia.com>
2026-09-04 14:02:05 -0600

    Revert "Correccion en la verificacion del tamaño de archivos (#65)"
    This reverts commit 420d405e3546b5ec9d24ac907a30aa48bb06f3ae.

--- honeycomb/forms/cells.py
+++ honeycomb/forms/cells.py
@@ -7,4 +7,3 @@ class AudioCellSchema(colander.MappingSchema):
     # data = colander.SchemaNode(deform.schema.FileData())
     mimetype = colander.SchemaNode(colander.String(), validator=colander.OneOf(['audio/mpeg', 'audio/aac', 'audio/ogg', 'audio/wav', 'audio/flac'], msg_err='"${val}" must be one of ${choices}'))
     parent = colander.SchemaNode(colander.String(), validator=colander.uuid, missing=None)
-    length = colander.SchemaNode(colander.Float(), missing=0.0)
